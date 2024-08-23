573e29e158a0d3c340f99f8f16ca29af1774313a
Darsi Sriguru Aakash <66508746+Aakashdarsi@users.noreply.github.com>
2024-08-23 21:06:44 +0800

    AssertsTimeout

--- src/test/java/QuickBeforeAfterTest.java
+++ src/test/java/QuickBeforeAfterTest.java
@@ -4,6 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import sg.nus.StringHelper;
 
+import java.time.Duration;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
 public class QuickBeforeAfterTest {
 
 
@@ -29,5 +35,25 @@ public class QuickBeforeAfterTest {
     @Test
     public void test2(){
         System.out.println("test2");
+    }
+
+    @Test
+    public void testArrayEquality(){
+        int[] arr1 = {1,3,2,4,5};
+        int[] exxpected = {1,2,3,4,5};
+        Arrays.sort(arr1);
+        assertArrayEquals(exxpected, arr1);
+    }
+    @Test()
+    public void testSort_Performance(){
+        int arr[]  = {12,23,4};
+
+        assertTimeout(Duration.ofSeconds(0),()->{
+            for(int i = 0; i < 1000000; i++){
+                arr[0] = i;
+                Arrays.sort(arr);
+            }
+        });
+
     }
 }
