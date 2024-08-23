dc48422785ae02f8e68b643d2bfa4cb20c1aea7b
Darsi Sriguru Aakash <66508746+Aakashdarsi@users.noreply.github.com>
2024-08-23 19:23:47 +0800

    Refactoring JUnit Test Cases

--- src/test/java/StringHelperTest.java
+++ src/test/java/StringHelperTest.java
@@ -6,19 +6,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StringHelperTest {
 
     @Test
-    public void help(){
+    public void testTrunecatedAInFirst2Positions_AInFirst2Positions(){
         StringHelper stringHelper = new StringHelper();
 
 
         // AACD -> CD, ACD => CD, CDEF => CDEF, CDAA => CDAA
 
        assertEquals("CD",stringHelper.truncateAInFirst2Positions("AACD"));
-       assertEquals("CD",stringHelper.truncateAInFirst2Positions("ACD"));
-       assertEquals("CDEF",stringHelper.truncateAInFirst2Positions("CDEF"));
-       assertEquals("CDAA",stringHelper.truncateAInFirst2Positions("CDAA"));
 
     }
 
+    @Test
+    public void testTrunecatedFirst2Positions_AInFirstPosition(){
+        StringHelper stringHelper = new StringHelper();
+        assertEquals("CD",stringHelper.truncateAInFirst2Positions("ACD"));
+    }
 
+    @Test
+    public  void testTruncatedInFirts2Positions_NoASInString(){
+        StringHelper stringHelper = new StringHelper();
+        assertEquals("CDEF",stringHelper.truncateAInFirst2Positions("CDEF"));
+    }
+
+    @Test
+    public void testTruncatedInFirst2Positions_AAtLast(){
+        StringHelper stringHelper = new StringHelper();
+        assertEquals("CDAA",stringHelper.truncateAInFirst2Positions("CDAA"));
+    }
 
 }
